7cc365d15252d81e6c29168ddaee3bb46b8d9dc1
c.spataro <c.spataro@tecso.biz>
2019-05-31 16:39:43 +0200

    fix-file
    Fix dei conflitti committati

--- esf-fitav-portlet/docroot/WEB-INF/src/it/ethica/esf/service/base/ESFCardServiceClpInvoker.java
+++ esf-fitav-portlet/docroot/WEB-INF/src/it/ethica/esf/service/base/ESFCardServiceClpInvoker.java
@@ -1,75 +1,3 @@
-<<<<<<< HEAD
-/**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- */
-
-package it.ethica.esf.service.base;
-
-import it.ethica.esf.service.ESFCardServiceUtil;
-
-import java.util.Arrays;
-
-/**
- * @author Ethica
- * @generated
- */
-public class ESFCardServiceClpInvoker {
-	public ESFCardServiceClpInvoker() {
-		_methodName378 = "getBeanIdentifier";
-
-		_methodParameterTypes378 = new String[] {  };
-
-		_methodName379 = "setBeanIdentifier";
-
-		_methodParameterTypes379 = new String[] { "java.lang.String" };
-	}
-
-	public Object invokeMethod(String name, String[] parameterTypes,
-		Object[] arguments) throws Throwable {
-		if (_methodName378.equals(name) &&
-				Arrays.deepEquals(_methodParameterTypes378, parameterTypes)) {
-			return ESFCardServiceUtil.getBeanIdentifier();
-		}
-
-		if (_methodName379.equals(name) &&
-				Arrays.deepEquals(_methodParameterTypes379, parameterTypes)) {
-			ESFCardServiceUtil.setBeanIdentifier((java.lang.String)arguments[0]);
-
-			return null;
-		}
-
-		throw new UnsupportedOperationException();
-	}
-
-	private String _methodName378;
-	private String[] _methodParameterTypes378;
-	private String _methodName379;
-	private String[] _methodParameterTypes379;
-=======
-/**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- */
-
 package it.ethica.esf.service.base;
 
 import it.ethica.esf.service.ESFCardServiceUtil;
@@ -112,5 +40,4 @@ public class ESFCardServiceClpInvoker {
 	private String[] _methodParameterTypes374;
 	private String _methodName375;
 	private String[] _methodParameterTypes375;
->>>>>>> 4e2f23245d33f5cc80bffcdd36acf34e75accc38
 }
